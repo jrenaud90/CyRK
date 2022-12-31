f1b30fd549d87211e1845fbb485ce471ba6974d8
cerrussell <80227828+cerrussell@users.noreply.github.com>
2022-12-31 00:00:51 -0500

    Fixed typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     ext_modules=[
         Extension(
             name='_cyrk.c',
-            sources=['CyRK/cy/_cyrk.c']),
-        include_dirs=[numpy.get_include()])
+            sources=['CyRK/cy/_cyrk.c'],
+            include_dirs=[numpy.get_include()])
     ]
 )
