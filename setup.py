413c10d8f74a1d33f723885e9d5cd4276c2e9ab1
Caroline Russell <80227828+cerrussell@users.noreply.github.com>
2022-12-30 23:55:15 -0500

    Fix for numpy error when compiling

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import Extension,setup
+from setuptools import Extension,setup,numpy
 
 setup(
     ext_modules=[
         Extension(
             name='_cyrk.c',
-            sources=['CyRK/cy/_cyrk.c'])
+            sources=['CyRK/cy/_cyrk.c']),
+        include_dirs=[numpy.get_include()])
     ]
 )
