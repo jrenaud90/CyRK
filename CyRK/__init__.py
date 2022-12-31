61268a0639ea0ab3079314efb5e199914b22a636
Jrenaud-Desk <joe.p.renaud@gmail.com>
2022-12-31 11:57:04 -0500

    added new diffeq function converter helper functions

--- CyRK/__init__.py
+++ CyRK/__init__.py
@@ -9,5 +9,8 @@ from .nb.nbrk import nbrk_ode
 # Import cython solver
 from CyRK.cy.cyrk import cyrk_ode
 
+# Import helper functions
+from .helper import nb2cy, cy2nb
+
 # Import test functions
 from ._test import test_cyrk, test_nbrk
