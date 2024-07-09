365b9c6870ff8ce4fbe207dbf7568fabf2e89312
Joe P. Renaud <joe.p.renaud@gmail.com>
2024-07-09 16:56:35 -0400

    Fixing CI

--- CyRK/utils/parallel.pyx
+++ CyRK/utils/parallel.pyx
@@ -3,12 +3,14 @@
 
 from cython.parallel import prange
 
+from libc.math cimport floor  
+
 cdef void f() noexcept nogil:
     cdef int i, j, k, x
     for i in prange(100_000):
         for j in range(i):
             for k in range(j):
-                x = i - j + int(k / 2)
+                x = i - j + <int>floor(k / 2)
     
 
 f()
