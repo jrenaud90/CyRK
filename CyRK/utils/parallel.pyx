61bec9e433e4212a1e437edae572c0b1516ab0eb
Joe P. Renaud <joe.p.renaud@gmail.com>
2024-07-09 16:48:13 -0400

    Fixing CI

---
+++ CyRK/utils/parallel.pyx
@@ -0,0 +1,15 @@
+# distutils: language = c++
+# cython: boundscheck=False, wraparound=False, nonecheck=False, cdivision=True, initializedcheck=False
+
+from cython.parallel import prange
+
+cdef void f():
+    cdef int i, j, k, x
+    for i in prange(100_000):
+        for j in range(i):
+            for k in range(j):
+                x = i - j + int(k / 2)
+    
+
+f()
+        
