e42cb3db2bc3a6f0416d2dbcd75574b76815b959
Jrenaud-Desk <joe.p.renaud@gmail.com>
2025-08-13 13:21:32 -0400

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,16 @@
 import sys
 import os
-import CyRK
 sys.path.insert(0, os.path.abspath('../CyRK'))
+import toml
 
-project = 'CyRK'
+pyproject_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml'))
+with open(pyproject_path, 'r') as f:
+    pyproject = toml.load(f)
+
+project = pyproject['project']['name']
+release = pyproject['project']['version']
+
 author = 'Joe P. Renaud'
-release = CyRK.version
 
 extensions = [
     'myst_parser',
